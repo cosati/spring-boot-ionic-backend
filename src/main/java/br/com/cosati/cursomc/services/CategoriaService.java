acbf0f9132821a45950fbffbbeea9f47b8e12fdf
Alexandre Cosati <cosati.alexandre@gmail.com>
2020-02-02 22:45:39 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/cosati/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/cosati/cursomc/services/CategoriaService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.cosati.cursomc.domain.Categoria;
 import br.com.cosati.cursomc.repositories.CategoriaRepository;
+import br.com.cosati.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,6 +16,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 	
