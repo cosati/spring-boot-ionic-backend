c7ecaabbe3df8121d99e4e8c168b7f2871c9ffb7
Alexandre Cosati <cosati.alexandre@gmail.com>
2020-03-24 22:00:50 -0300

    Adicionando senha a Cliente

--- src/main/java/br/com/cosati/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/br/com/cosati/cursomc/dto/ClienteNewDTO.java
@@ -24,6 +24,9 @@ public class ClienteNewDTO implements Serializable {
 	//@CPF ou @CNPJ
 	private String cpfOuCnpj;
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
@@ -148,6 +151,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 }
--- src/main/java/br/com/cosati/cursomc/services/DBService.java
+++ src/main/java/br/com/cosati/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.cosati.cursomc.domain.Categoria;
@@ -51,6 +52,8 @@ public class DBService {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private BCryptPasswordEncoder pe;
 
 	public void instantiateTestDatabase () throws ParseException  {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -197,11 +200,11 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOAFISICA/*, pe.encode("123")*/);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA/*, pe.encode("123")*/);
+		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 		//cli2.addPerfil(Perfil.ADMIN);
 		
