045e45b02cd2a85009e84d216c7a81cc8972d80a
Alexandre Cosati <cosati.alexandre@gmail.com>
2020-03-26 21:24:55 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/br/com/cosati/cursomc/services/PedidoService.java
+++ src/main/java/br/com/cosati/cursomc/services/PedidoService.java
@@ -3,9 +3,13 @@ package br.com.cosati.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.cosati.cursomc.domain.Cliente;
 import br.com.cosati.cursomc.domain.ItemPedido;
 import br.com.cosati.cursomc.domain.PagamentoComBoleto;
 import br.com.cosati.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import br.com.cosati.cursomc.repositories.ClienteRepository;
 import br.com.cosati.cursomc.repositories.ItemPedidoRepository;
 import br.com.cosati.cursomc.repositories.PagamentoRepository;
 import br.com.cosati.cursomc.repositories.PedidoRepository;
+import br.com.cosati.cursomc.security.UserSS;
+import br.com.cosati.cursomc.services.exceptions.AuthorizationException;
 import br.com.cosati.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -72,6 +78,16 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado.");
+		}		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
