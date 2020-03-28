e52ca35eeb32dae320b7cbfc9aacab24af2b719b
Alexandre Cosati <cosati.alexandre@gmail.com>
2020-03-28 20:38:00 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/cosati/cursomc/CursomcApplication.java
+++ src/main/java/br/com/cosati/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package br.com.cosati.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.cosati.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,6 +19,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadfFile("D:\\Curso Java\\Fotos\\evento2.jpg");
 	}
 
 }
